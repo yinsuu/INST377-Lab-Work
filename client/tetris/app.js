28aa37aba2ce52fdf35f050211469519064a30e6
Jacky <jackychen5800@gmail.com>
2022-02-18 18:37:27 -0500

    drawing/removing tetrominoes

--- client/tetris/app.js
+++ client/tetris/app.js
@@ -1,7 +1,68 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid')
-    let squares = Array.from(document.querySelectorAll('.grid div'))
-    const ScoreDisplay = document.querySelector('#score')
-    const StartBtn = document.querySelector('#start-button')
-    const width = 10 
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+  const width = 10 
+
+  // The Tetraominoes
+  const lTertromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+1],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2],
+  ]
+
+  const zTertromino = [
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+  ]
+
+  const tTertromino = [
+    [1, width, width+1, width+2],
+    [1, width+1, width+2, width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+1],
+  ]
+
+  const oTertromino = [
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+  ]
+
+  const iTertromino = [
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+  ]
+
+  const theTetrominoes = [lTertromino, zTertromino, tTertromino, oTertromino, iTertromino]
+
+  let currentPosition = 4
+  let currentRotation = 0
+
+  // randomly select a Tetromino and its first rotation
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  let current = theTetrominoes[random][currentRotation]
+
+  // draw the Teromino
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino')
+    })
+  }
+
+  // undraw the Teromino
+  function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino')
+    })
+  }
+
+  
 })
