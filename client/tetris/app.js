7bdc95dbf8f7d2ee547e02b286ab63b7fa42b779
Jacky <jackychen5800@gmail.com>
2022-02-18 18:56:58 -0500

    move/freeze function

--- client/tetris/app.js
+++ client/tetris/app.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid')
-  let squares = Array.from(document.querySelectorAll('.grid div'))
-  const ScoreDisplay = document.querySelector('#score')
-  const StartBtn = document.querySelector('#start-button')
-  const width = 10 
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score');
+  const StartBtn = document.querySelector('#start-button');
+  const width = 10;
 
   // The Tetraominoes
   const lTertromino = [
@@ -11,58 +11,79 @@ document.addEventListener('DOMContentLoaded', () => {
     [width, width+1, width+2, width*2+1],
     [1, width+1, width*2+1, width*2],
     [width, width*2, width*2+1, width*2+2],
-  ]
+  ];
 
   const zTertromino = [
     [0, width, width+1, width*2+1],
     [width+1, width+2, width*2, width*2+1],
     [0, width, width+1, width*2+1],
     [width+1, width+2, width*2, width*2+1],
-  ]
+  ];
 
   const tTertromino = [
     [1, width, width+1, width+2],
     [1, width+1, width+2, width*2+1],
     [width, width+1, width+2, width*2+1],
     [1, width, width+1, width*2+1],
-  ]
+  ];
 
   const oTertromino = [
     [0, 1, width, width+1],
     [0, 1, width, width+1],
     [0, 1, width, width+1],
     [0, 1, width, width+1],
-  ]
+  ];
 
   const iTertromino = [
     [1, width+1, width*2+1, width*3+1],
     [width, width+1, width+2, width+3],
     [1, width+1, width*2+1, width*3+1],
     [width, width+1, width+2, width+3],
-  ]
+  ];
 
   const theTetrominoes = [lTertromino, zTertromino, tTertromino, oTertromino, iTertromino]
 
-  let currentPosition = 4
-  let currentRotation = 0
+  let currentPosition = 4;
+  let currentRotation = 0;
 
   // randomly select a Tetromino and its first rotation
-  let random = Math.floor(Math.random()*theTetrominoes.length)
-  let current = theTetrominoes[random][currentRotation]
+  let random = Math.floor(Math.random()*theTetrominoes.length);
+  let current = theTetrominoes[random][currentRotation];
 
   // draw the Teromino
   function draw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.add('tetromino')
-    })
+    });
   }
 
   // undraw the Teromino
   function undraw() {
     current.forEach(index => {
-      squares[currentPosition + index].classList.remove('tetromino')
-    })
+      squares[currentPosition + index].classList.remove('tetromino');
+    });
   }
 
+  // make the tetromino move down every second
+  timerId = setInterval(moveDown, 1000);
+
+  // move down function
+  function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+  }
   
+  // freeze function
+  function freeze() {
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+      // start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
+  }
 })
