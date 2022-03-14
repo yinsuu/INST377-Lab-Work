24a9b811736886a8581388a3bd95980b720265a4
Jacky <jackychen5800@gmail.com>
2022-03-13 22:42:47 -0400

    lab update

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,14 +1,24 @@
+function dataHandler() {
+  console.table(arrayFromJson.data); // this is called "dot notation"
+}
+// As the last step of your lab, hook this up to index.html
+
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form');
+  const submit = document.querySelector('.submit');
+  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  
+  if (arrayFromJson.data.lenth > 0) { 
   form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
     submitEvent.preventDefault(); // This prevents your page from refreshing!
     console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
+    submit.style.display = 'block'
+    dataHandler(arrayFromJson.data);
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
     // it contains all 1,000 records we need
-  });
+    });
+  }
 }
 
 // this actually runs first! It's calling the function above
