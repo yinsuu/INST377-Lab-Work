ef7ce3bc03d14eac51a0c1dbdf43b15cde0efd89
Jacky <jackychen5800@gmail.com>
2022-03-13 23:28:46 -0400

    lab update

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,20 +1,49 @@
-function dataHandler() {
-  console.table(arrayFromJson.data); // this is called "dot notation"
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(
+    Math.random() * (newMax - newMin + 1) + newMin
+  );
+}
+function dataHandler(dataArray) {
+  // console.table(dataArray.data); // this is called "dot notation"
+  const range = [...Array(15).keys()];
+  const listItems = range.map((item, index) => {
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+    return dataArray[restNum];
+  });
+  return listItems;
+  // range.forEach((item) => {
+  //  console.log('range item', item);
+  // });
+}
+
+function createHtmlList() {
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
 }
 // As the last step of your lab, hook this up to index.html
 
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form');
   const submit = document.querySelector('.submit');
+  submit.style.display = 'none';
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
   
-  if (arrayFromJson.data.lenth > 0) { 
+  if (arrayFromJson.data.length > 0) { 
+  submit.style.display = 'block';
   form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
     submitEvent.preventDefault(); // This prevents your page from refreshing!
     console.log('form submission'); // this is substituting for a "breakpoint"
-    submit.style.display = 'block'
-    dataHandler(arrayFromJson.data);
+    const restoArray = dataHandler(arrayFromJson.data);
+    createHtmlList(restoArray);
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
     // it contains all 1,000 records we need
     });
