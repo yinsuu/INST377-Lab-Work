949e17404b4c21bd242550a503d877737b16bbf7
Jacky <jackychen5800@gmail.com>
2022-03-13 23:33:08 -0400

    lab 6 update

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -18,7 +18,7 @@ function dataHandler(dataArray) {
   // });
 }
 
-function createHtmlList() {
+function createHtmlList(collection) {
   const targetList = document.querySelector('.resto-list');
   targetList.innerHTML = '';
   collection.forEach((item) => {
@@ -36,14 +36,13 @@ async function mainEvent() { // the async keyword means we can make API requests
   submit.style.display = 'none';
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-  
-  if (arrayFromJson.data.length > 0) { 
-  submit.style.display = 'block';
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const restoArray = dataHandler(arrayFromJson.data);
-    createHtmlList(restoArray);
+  if (arrayFromJson.data.length > 0) {
+    submit.style.display = 'block';
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a "breakpoint"
+      const restoArray = dataHandler(arrayFromJson.data);
+      createHtmlList(restoArray);
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
     // it contains all 1,000 records we need
     });
