80f0d10b93f1285cfa236238adf2787dd42a80ad
Jacky <jackychen5800@gmail.com>
2022-03-26 21:52:10 -0400

    initial commit

---
+++ client/lab_7/script.js
@@ -0,0 +1,53 @@
+function getRandomIntInclusive(min, max) {
+    const newMin = Math.ceil(min);
+    const newMax = Math.floor(max);
+    return Math.floor(
+      Math.random() * (newMax - newMin + 1) + newMin
+    );
+  }
+  function dataHandler(dataArray) {
+    // console.table(dataArray.data); // this is called "dot notation"
+    const range = [...Array(15).keys()];
+    const listItems = range.map((item, index) => {
+      const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+      return dataArray[restNum];
+    });
+    return listItems;
+    // range.forEach((item) => {
+    //  console.log('range item', item);
+    // });
+  }
+  
+  function createHtmlList(collection) {
+    const targetList = document.querySelector('.resto-list');
+    targetList.innerHTML = '';
+    collection.forEach((item) => {
+      const {name} = item;
+      const displayName = name.toLowerCase();
+      const injectThisItem = `<li>${displayName}</li>`;
+      targetList.innerHTML += injectThisItem;
+    });
+  }
+  // As the last step of your lab, hook this up to index.html
+  
+  async function mainEvent() { // the async keyword means we can make API requests
+    const form = document.querySelector('.main_form');
+    const submit = document.querySelector('.submit');
+    submit.style.display = 'none';
+    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+    if (arrayFromJson.data.length > 0) {
+      submit.style.display = 'block';
+      form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+        submitEvent.preventDefault(); // This prevents your page from refreshing!
+        console.log('form submission'); // this is substituting for a "breakpoint"
+        const restoArray = dataHandler(arrayFromJson.data);
+        createHtmlList(restoArray);
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+      });
+    }
+  }
+  
+  // this actually runs first! It's calling the function above
+  document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
